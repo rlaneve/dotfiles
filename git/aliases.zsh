50cbeea67e1ae8fab0f64c4bc197f90d385e040d
Sumeet Jain <sumeetjain@sumeet-jains-macbook.local>
2010-08-21 14:14:33 -0700

    Adding  which adds, commits, and pushes everything in a dirty git repository at once.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,3 +8,40 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+# Adds, commits, and pushes everything. Example: 'gpa "This is easy as pie!"'
+# Modified from http://mattsears.com/2008/8/11/quick-git-add-commit-push-and-deploy
+gpa() {
+    # Check if the repo is dirty
+    st=$(git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]
+        then
+        echo ""
+    else
+        if [[ $st == "nothing to commit (working directory clean)" ]]
+            then
+            echo "Nothing to push"
+        else
+            # Make sure there is a message with the commit
+            if [ -z "$1" ]
+                then
+                echo "You must specify a message with your commit"
+                return
+            fi
+
+            # Commit all the changes by default
+            echo "Adding new files to Git repository"
+            git add .
+
+            echo "Commiting to local Git repository"
+            git commit -a -m "$1"
+
+            # Push changes if a remote repository exists
+            if git remote | grep -q "origin"
+                then
+                echo "Pushing changes to remote repository"
+                git push origin HEAD
+            fi
+        fi
+    fi
+}
